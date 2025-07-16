eff6bff11c9aa9b6b8d48570a7972a811c0cd3ca
Nabila Ayaba <nabilaayaba@gmail.com>
2025-07-16 14:28:56 +0300

    Create index view and controller

--- controllers/listing.controller.js
+++ controllers/listing.controller.js
@@ -10,8 +10,20 @@ router.get('/new', (req, res) => {
 
 // POST FORM DATA TO DATABASE
 router.post('/', async (req, res) => {
-    await Listing.create(req.body)
-    res.send('you submitted the form')
+    try {
+        await Listing.create(req.body)
+        res.redirect('/listings')
+    } catch (error) {
+        console.log(error)
+        res.send('Something went wrong')
+    }
+})
+
+// VIEW THE INDEX PAGE
+router.get('/', async (req, res) => {
+    const foundListings = await Listing.find()
+    console.log(foundListings)
+    res.render('listings/index.ejs', { foundListings: foundListings })
 })
 
 module.exports = router
